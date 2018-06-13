d525111652bbba73c41bac57a5e53ccf20743460
Daniel Gutierrez <dgutierrez@gorebiobio.cl>
2018-06-13 17:10:54 -0400

    OK_REST_Clave

--- resources/views/vendor/adminlte/auth/passwords/reset.blade.php
+++ resources/views/vendor/adminlte/auth/passwords/reset.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlte::layouts.auth')
 
 @section('htmlheader_title')
-    Password reset
+    Cambio de Contraseña
 @endsection
 
 @section('content')
@@ -10,64 +10,62 @@
 
     <div id="app">
         <div class="login-box">
-        <div class="login-logo">
-            <center><img src="/img/logo.png" alt=""></center>
-        </div><!-- /.login-logo -->
+            <div class="login-logo">
+                <center><img src="/img/logo.png" alt=""></center>
+            </div><!-- /.login-logo -->
 
-        @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-        <div class="login-box-body">
-            <p class="login-box-msg">{{ trans('adminlte_lang::message.passwordreset') }}AA</p>
-            <form action="{{ url('/password/reset') }}" method="post">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                <input type="hidden" name="token" value="{{ $token }}">
-                <div class="form-group has-feedback">
-                    <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}"/>
-                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
                 </div>
+            @endif
 
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Password" name="password"/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> {{ trans('adminlte_lang::message.someproblems') }}<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
+            @endif
 
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" placeholder="Password" name="password_confirmation"/>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
+            <div class="login-box-body">
+                <p class="login-box-msg">REESTABLECER CONTRASEÑA</p>
+                <form action="{{ url('/password/reset') }}" method="post">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    <div class="form-group has-feedback">
+                        <input type="email" class="form-control" placeholder="Correo Electrónico" name="email"
+                               value="{{ old('email') }}"/>
+                        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    </div>
 
-                <div class="row">
-                    <div class="col-xs-2">
-                    </div><!-- /.col -->
-                    <div class="col-xs-8">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.passwordreset') }}</button>
-                    </div><!-- /.col -->
-                    <div class="col-xs-2">
-                    </div><!-- /.col -->
-                </div>
-            </form>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" placeholder="Nueva Clave" name="password"/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
 
-            <a href="{{ url('/login') }}">Log in</a><br>
-            <a href="{{ url('/register') }}" class="text-center">{{ trans('adminlte_lang::message.membreship') }}</a>
+                    <div class="form-group has-feedback">
+                        <input type="password" class="form-control" placeholder="Repita Nueva Clave"
+                               name="password_confirmation"/>
+                        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    </div>
 
-        </div><!-- /.login-box-body -->
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <button type="submit"
+                                    class="btn btn-primary btn-block btn-flat">{{ trans('adminlte_lang::message.passwordreset') }}</button>
+                        </div><!-- /.col -->
+                    </div>
+                </form>
+                <br>
+                <a href="{{ url('/login') }}">Cancelar cambio de clave</a><br>
 
-    </div><!-- /.login-box -->
+            </div><!-- /.login-box-body -->
+
+        </div><!-- /.login-box -->
     </div>
 
     @include('adminlte::layouts.partials.scripts_auth')
