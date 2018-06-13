e856ea86a408f0698e261df2d0d5e04054d0be88
Daniel Gutierrez <dgutierrez@gorebiobio.cl>
2018-06-13 15:16:04 -0400

    Fix_Mail

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.gorebiobio.cl'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'plataforma@gorebiobio.cl'),
+        'name' => env('MAIL_FROM_NAME', 'Plataforma Informática'),
     ],
 
     /*
